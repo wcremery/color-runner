265afe7e6897371f705f2024446e1761852bd502
82L <pblefloch@gmail.com>
2021-03-26 14:51:22 +0100

    Added straight jump
    TODO : add analogic + add collision detection

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private LayerMask platformsLayerMask;
+    public Rigidbody2D rigidbody2D;
+    public BoxCollider2D boxCollider2D;
+    private float jumpVelocity = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.velocity = Vector2.up * 6f;
+        }
+    }
+    private bool IsGrounded()
+    {
+       RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
+       return raycastHit2D.collider != null;
+    }
+    private bool IsAgainstWall()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.left, 0.1f, platformsLayerMask);
+        return raycastHit2D.collider != null;
+    }
+}
