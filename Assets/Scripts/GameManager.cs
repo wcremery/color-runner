f51d2608fb29de79d597ad87bfcf2361a2aa9b17
82L <pblefloch@gmail.com>
2021-03-30 08:13:30 +0200

    Limit reset put back to 1000

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Player player;
-    private const int RESETLIMIT = 20;
+    private const int RESETLIMIT = 1000;
     void Start()
     {
         
