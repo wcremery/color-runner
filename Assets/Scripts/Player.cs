ae700e5d55067f75c44ad66d96a8dd873ca86543
82L <pblefloch@gmail.com>
2021-04-09 19:26:51 +0200

    [Added] Adaptative Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,13 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public BoxCollider2D boxCollider2D;
     public GameManager gameManager;
+    public float timeLimitJump = 0.2f;
     private float jumpVelocity = 6f;
     private float moveSpeed =1f;
     public ColorType colorType;
     private int controlsColorNumber = 0;
+    private float timeJump;
+    
 
     // Start is called before the first frame update
 
@@ -36,7 +39,7 @@ public class Player : MonoBehaviour
     }
     private bool IsGrounded()
     {
-       RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.5f, platformsLayerMask);
+       RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
         if (raycastHit2D.collider != null)
         {
             Debug.DrawLine(gameObject.transform.position, raycastHit2D.point, Color.green, 100f);
@@ -80,12 +83,24 @@ public class Player : MonoBehaviour
         
         gameManager.GameOver();
     }
-    public void OnJump()
+    public void OnJump(InputValue input)
     {
         Debug.Log("[Controls] jump");
-        if (IsGrounded())
+        if (IsGrounded() && input.isPressed)
         {
+
             myRigidbody.velocity = Vector2.up * jumpVelocity;
+            timeJump = Time.time;
+
+        }
+        else
+        {
+            //Debug.Log(timeJump +" " + timeLimitJump + " " + " ");
+            if (timeJump + timeLimitJump > Time.time)
+            {
+                Debug.Log("[Controls] Small Jump");
+                myRigidbody.velocity += Vector2.down * jumpVelocity/2;
+            }
         }
         
     }
